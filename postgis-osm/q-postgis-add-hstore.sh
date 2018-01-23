f6f984f0891e653390a4f4635193a93a1a36ab9e
Sergei Beilin <sergei.beilin@treqster.net>
2018-01-23 23:48:40 +0100

    docker-compose fixes

--- postgis-osm/q-postgis-add-hstore.sh
+++ postgis-osm/q-postgis-add-hstore.sh
@@ -6,9 +6,9 @@ export PGUSER="$POSTGRES_USER"
 
 "${psql[@]}" -c "CREATE EXTENSION hstore;"
 
-for DB in $POSTGRES_DBS; do
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE $DB template template_postgis;" 
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --database $DB -c "CREATE EXTENSION hstore;" 
-done
+#for DB in $POSTGRES_DBS; do
+#  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE $DB template template_postgis;" 
+#  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname $DB -c "CREATE EXTENSION hstore;" 
+#done
 
 
